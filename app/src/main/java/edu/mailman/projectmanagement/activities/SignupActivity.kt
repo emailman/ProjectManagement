02078fc323b93cfea26adbae2539a1ba5f2f9df9
Eric Mailman <emailman@dtcc.edu>
2023-02-13 16:53:37 -0500

    Create Firebase account for user

--- app/src/main/java/edu/mailman/projectmanagement/activities/SignupActivity.kt
+++ app/src/main/java/edu/mailman/projectmanagement/activities/SignupActivity.kt
@@ -1,8 +1,10 @@
 package edu.mailman.projectmanagement.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowInsets
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import edu.mailman.projectmanagement.R
 import edu.mailman.projectmanagement.databinding.ActivitySignupBinding
 
@@ -29,6 +31,61 @@ class SignupActivity : BaseActivity() {
             binding?.toolbarSignUpActivity?.setNavigationOnClickListener {
                 onBackPressedDispatcher.onBackPressed()
             }
+
+            binding?.btnSignUp?.setOnClickListener {
+                registerUser()
+            }
+        }
+    }
+
+    private fun registerUser() {
+        val name = binding?.etName?.text?.toString()?.trim {it <= ' '}
+        val email = binding?.etEmail?.text?.toString()?.trim {it <= ' '}
+        val password = binding?.etPassword?.text?.toString()?.trim {it <= ' '}
+
+        if (validateForm(name, email, password)) {
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().
+            createUserWithEmailAndPassword(email!!, password!!).addOnCompleteListener { task ->
+                hideProgressDialog()
+                if (task.isSuccessful) {
+                    val firebaseUser = task.result!!.user!!
+                    val registeredEmail = firebaseUser.email
+                    Toast.makeText(
+                        this,
+                        "$name successfully registered $registeredEmail",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    FirebaseAuth.getInstance().signOut()
+                    finish()
+
+                } else {
+                    Toast.makeText(
+                        this,
+                        task.exception!!.message, Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    private fun validateForm(name: String?, email: String?, password: String?) : Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter an email address")
+                false
+            }
+
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> { true }
         }
     }
 
