9a940117e31eb7b71a7a294e3e0c17a14532fd29
Eric Mailman <emailman@dtcc.edu>
2023-02-22 17:57:11 -0500

    Choose new picture for profile

--- app/src/main/java/edu/mailman/projectmanagement/activities/MyProfileActivity.kt
+++ app/src/main/java/edu/mailman/projectmanagement/activities/MyProfileActivity.kt
@@ -1,14 +1,33 @@
 package edu.mailman.projectmanagement.activities
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import edu.mailman.projectmanagement.R
 import edu.mailman.projectmanagement.databinding.ActivityMyProfileBinding
 import edu.mailman.projectmanagement.firestore.FirestoreClass
 import edu.mailman.projectmanagement.models.User
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
     private var binding: ActivityMyProfileBinding? = null
+
+    companion object {
+        private const val READ_STORAGE_PERMISSION_CODE = 1
+        private const val PICK_IMAGE_REQUEST_CODE = 2
+    }
+
+    private var selectedImageFileUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +37,67 @@ class MyProfileActivity : BaseActivity() {
         setupActionBar()
 
         FirestoreClass().loadUserData(this)
+
+        binding?.ivUserImage?.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+                Log.i("eric", "permission was granted")
+
+            } else {
+                ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_STORAGE_PERMISSION_CODE )
+                Log.i("eric", "permission was requested")
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == READ_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showImageChooser()
+
+            } else {
+                Toast.makeText(this,"Permission not allowed," +
+                        " but you can allow it from Settings", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun showImageChooser() {
+        val galleryIntent = Intent(Intent.ACTION_PICK,
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == Activity.RESULT_OK &&
+                requestCode == PICK_IMAGE_REQUEST_CODE &&
+                data!!.data != null) {
+            selectedImageFileUri = data.data
+
+            try {
+                Glide
+                    .with(this@MyProfileActivity)
+                    .load(selectedImageFileUri)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(binding?.ivUserImage as ImageView)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun setupActionBar() {
@@ -48,8 +128,5 @@ class MyProfileActivity : BaseActivity() {
         if (user.mobile != 0L) {
             binding?.etMobile?.setText(user.mobile.toString())
         }
-
-
-
     }
 }
